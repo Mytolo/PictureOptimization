0e23ea07205a8807e89e89ea1f37c5f686770959
Mytolo <panajiotis.christoforidis@freenet.de>
2018-03-27 14:12:56 +0200

    fix dirErrors

--- optimiseImages.sh
+++ optimiseImages.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 curDir=$(pwd|rev|cut -d "/" -f1 |rev)
-ConvertDir="./ConvertedImages$curDir"
+ConvertDir="../${curDir}Converted"
 mkdir -p $ConvertDir
 ls $ConvertDir
 
 optimiseJpg() {
 for i in $(find . -iname "*.jpg" -type f | grep -v Unoptimized)
  do 
-   name=$(echo $i|cut -d "/" -f2)
+   name=$(echo $i|cut -d "/" -f2-)
    echo guetzli --nomemlimit $i $ConvertDir/$name
    guetzli --nomemlimit $i $ConvertDir/$name
 done
@@ -17,7 +17,7 @@ done
 optimisePng() {
   for i in $(find . -iname "*.png" -type f | grep -v Unoptimized)
   do 
-    name=$(echo $i|cut -d "/" -f2)
+    name=$(echo $i|cut -d "/" -f2-)
     echo pngquant $i 128 $ConvertDir/$name 
   done
 }
