3282460c441eb162f2cd7e67d49074ccb0926d76
Mytolo <mytolo@KororaLeopold.speedport.ip>
2018-03-27 17:48:24 +0200

    added parallelization

--- optimiseImages.sh
+++ optimiseImages.sh
@@ -2,21 +2,33 @@
 
 curDir=$(pwd|rev|cut -d "/" -f1 |rev)
 ConvertDir="../${curDir}Converted"
+CORES=$(nproc)
 mkdir -p $ConvertDir
 ls $ConvertDir
+#right settings for printf
+export LC_NUMERIC="en_US.UTF-8"
 
 optimiseJpg() {
 for i in $(find . -iname "*.jpg" -type f | grep -v Unoptimized)
  do 
    name=$(echo $i|cut -d "/" -f2-)
-   echo guetzli --nomemlimit $i $ConvertDir/$name
    if [ ! -d $(dirname $ConvertDir/$name) ]
    then
      mkdir -p $(dirname $ConvertDir/$name)
    fi
+   load=$(ps aux | grep guetzli | wc -l)
+   let load=$load-1
+   while [[ $load -ge $CORES ]]
+   do
+      load=$(ps aux | grep guetzli | wc -l)
+      let load=$load-1
+      printf "\n\rload to high ($load)"
+      sleep 0.1 
+   done
    if [ ! -f $ConvertDir/$name ]
    then
-        guetzli --nomemlimit $i $ConvertDir/$name
+   	echo guetzli --nomemlimit $i $ConvertDir/$name
+        guetzli --nomemlimit $i $ConvertDir/$name&
    fi
 done
 }
@@ -24,8 +36,26 @@ done
 optimisePng() {
   for i in $(find . -iname "*.png" -type f | grep -v Unoptimized)
   do 
-    name=$(echo $i|cut -d "/" -f2-)
-    echo pngquant $i 128 $ConvertDir/$name 
+   name=$(echo $i|cut -d "/" -f2-)
+   if [ ! -d $(dirname $ConvertDir/$name) ]
+   then
+     mkdir -p $(dirname $ConvertDir/$name)
+   fi
+   # use all cores equally
+   load=$(ps aux | grep guetzli | wc -l)
+   let load=$load-1 # one line additional false pos.
+   while [[ $load -ge $CORES ]]
+   do
+      load=$(ps aux | grep guetzli | wc -l)
+      let load=$load-1
+      printf "\n\rload to high ($load)"
+      sleep 0.1 
+   done
+   if [ ! -f $ConvertDir/$name ]
+   then
+    	echo pngquant --skip-if-larger -o $ConvertDir/$name --strip 256 $i
+        pngquant --skip-if-larger -o $ConvertDir/$name --strip 256 $i
+   fi
   done
 }
 
